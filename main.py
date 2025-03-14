0ecc7d8788094fcc117862c9f0b59a0eb1c4b35a
alexsaunderswps <alex.saunders@wildlifeprotectionsolutions.org>
2025-03-14 11:57:04 -0600

    matches course work now

--- main.py
+++ main.py
@@ -1,46 +1,41 @@
-#main.py
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
-    num_words = book_words(text)
-    char_library = book_characters(text)
-    alpha_library = alpha_report(char_library)
-    report = generate_report(book_path, num_words, alpha_library)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-        
-def book_words(text):
-    words = str(text).split()
-    return len(words)
-        
-def book_characters(text):
-    characters = {}
-    for char in text:
-        if not char.lower() in characters:
-            characters[char.lower()] = 1
-        else:
-            characters[char.lower()] += 1
-    return characters  
 
-def alpha_report(characters):
-    alpha_list = {}
-    for char,value in characters.items():
-        if char.isalpha():
-            alpha_list[char] = value
-    return alpha_list
 
-def generate_report(book, word_count, alpha_library):
-    report = []
-    file_name = "".join(book.split("/")[-1::])
-    title = "".join(file_name.split(".")[0:1]).capitalize()
-    report.append(f"--- Begin report of {file_name} ---\n")
-    report.append(f"{word_count} words found in the document\n\n")
-    for key,value in alpha_library.items():
-        report.append(f"The '{key}' character was found {value} times\n")
-    report.append(f"--- End report ---")
-    return " ".join(report)
-        
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
 main()
